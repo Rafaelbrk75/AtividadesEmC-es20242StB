60515ae6e45774bf0516b90149dbec39195085b2
Rafael Rodrigues <88215458+Rafaelbrk75@users.noreply.github.com>
2024-08-29 20:48:09 -0300

    Update atvCrescente.cpp

--- atividades/atvCrescente.cpp
+++ atividades/atvCrescente.cpp
@@ -1,14 +1,21 @@
 #include <stdio.h>
-//Peça 5 números ao usuário e depois apresente eles em ordem crescente.
-//Entrada: pedir 5 números
-//Saída: apresentar eles em ordem crescente
+//PeÃ§a 5 nÃºmeros ao usuÃ¡rio e depois apresente eles em ordem crescente.
+//Entrada: pedir 5 nÃºmeros
+//SaÃ­da: apresentar eles em ordem crescente
 int main(){
     int n[5];
     int i;
+   int tmp;
 
     for(i = 0; i < 5; i++){
         printf("Informe o %d numero: ", i+1);
         scanf("%d",&n[i]);
+
+    if(n[i] > n[i]) 
+      tmp = n[i]
+      n[i] = n[i]
+      n[i] = temp
     }
+
 
 }
